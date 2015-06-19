25919fae14d015e91404751442ed9f2d6e90ae9a
bestisoft <admin@bestisoft.com>
2015-06-19 10:39:47 +0200

    v0.0.1

---
+++ assets/js/pages/page_coming_soon.js
@@ -0,0 +1,10 @@
+var PageComingSoon = function () {
+
+    return {
+        //Coming Soon
+        initPageComingSoon: function () {
+          $('#defaultCountdown').countdown({until: new Date(2015, 8, 1)})
+        }
+
+    };
+}();        
